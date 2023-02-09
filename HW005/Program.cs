5f125e8e92d36ede51fb958e5d326dc7872b69ed
Ivan_Ladygin <ivan_ladiygin@mail.ru>
2023-02-09 23:10:21 +0500

    perhaps a working program has been created005

--- HW005/Program.cs
+++ HW005/Program.cs
@@ -1,20 +1,28 @@
 ﻿// Показать треугольник Паскаля *Сделать вывод в виде равнобедренного треугольника
+Console.WriteLine("Enter row value : ");
+int num = int.Parse(Console.ReadLine()??"0");
+int[][]mass = new int[num][];
 
-    Console.WriteLine("Enter row value : ");
-    int num = Convert.ToInt32(Console.ReadLine());
-
-    for (int i = 0; i < num; i++)//row
-    {
-        for (int j = num; j > i; j--)//spaces
-        {
-            Console.Write(" ");
-        }
-        int val = 1;
+for (int i = 0; i < mass.Length; i++)
+{
+    int[] result = new int[i+1];
         for (int j = 0; j <= i; j++)
         {
-            Console.Write(val + " ");
-            val = val * (i - j) / (j + 1);
+            if (j!=0&&j!=i)
+            {
+                result[j] = mass[i-1][j-1]+mass[i-1][j];
+            }
+            else
+            {
+                result[j] = 1;
+            }
         }
-        Console.WriteLine();
-    }
-    Console.ReadLine();
+    mass[i] = result;
+}
+Console.Clear();
+foreach (var item in mass)
+{
+    foreach(var item1 in item)
+        Console.Write($"{item1} ");
+    Console.WriteLine();
+}

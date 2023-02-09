8ca7aede4aae6a7c91023e65eb3a310d222c2667
Ivan_Ladygin <ivan_ladiygin@mail.ru>
2023-02-09 22:31:32 +0500

    perhaps a working program has been created005

---
+++ HW005/Program.cs
@@ -0,0 +1,20 @@
+﻿// Показать треугольник Паскаля *Сделать вывод в виде равнобедренного треугольника
+
+    Console.WriteLine("Enter row value : ");
+    int num = Convert.ToInt32(Console.ReadLine());
+
+    for (int i = 0; i < num; i++)//row
+    {
+        for (int j = num; j > i; j--)//spaces
+        {
+            Console.Write(" ");
+        }
+        int val = 1;
+        for (int j = 0; j <= i; j++)
+        {
+            Console.Write(val + " ");
+            val = val * (i - j) / (j + 1);
+        }
+        Console.WriteLine();
+    }
+    Console.ReadLine();
